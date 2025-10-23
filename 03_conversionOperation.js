a80f9c190cab5cdbd32367b4aca08c92c992e98a
luvanidixit101 <luvanidixit101@gmail.com>
2025-10-23 20:59:40 +0530

    comarision

--- 03_conversionOperation.js
+++ 03_conversionOperation.js
@@ -34,3 +34,51 @@ let stringNumber =String(someNumber)
 console.log(typeof someNumber)
 console.log(typeof stringNumber)
 console.log(stringNumber)
+
+//******************** operations ************************
+
+
+let value = 3
+let negvalue = -value
+console.log(negvalue);
+// negative values 
+
+//console.log(2+2);
+//console.log(2-2);
+//console.log(2*2);
+//console.log(2**2);
+//console.log(2/2);
+//console.log(2%3);
+
+let str1 ="hello"
+let str2 =" wold"
+
+let str3 =str1 + str2
+
+console.log(str3);
+
+console.log(str3 + 1 + 1);
+
+// that is diffrente  output value 
+
+console.log(1 + 1 + str3);6
+
+console.log((2 + 2) * 3 % 4);
+
+console.log(10 + true); // output 11
+
+console.log(+""); // output 0
+
+let num1,num2,num3
+
+num1= num2 = num3 = 2 + 2
+
+
+let gameCounter = 100
+gameCounter++;       // use to plus operater  in postfix
+
+++gameCounter ;   //use to plus operater in postfix
+
+console.log(gameCounter)  //output 102 
+
+
