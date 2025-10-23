bd281606e1e93841924e7ab22737bb4dee7e6123
luvanidixit101 <luvanidixit101@gmail.com>
2025-10-23 21:28:56 +0530

    05

---
+++ 05_dataType_basic_summary.js
@@ -0,0 +1,66 @@
+// primitive 
+
+// 7 types
+
+// String
+// Number
+// Boolean
+// null
+// undefined
+// Symbol
+// BigInt
+
+const score = 300
+const score1 = 1003.3
+
+const isLoggedIn =false;
+const scoreValue =null;
+
+const id =Symbol('123')
+const anoter =Symbol('123')
+
+console.log(id === anoter) // false
+
+
+const bigNumber = 123456782225n
+
+
+
+
+
+// Reference type  or non primitive
+
+// Array, Objects, Functions
+
+
+// array declare 
+const heros = ["shaktiman", "nagraj","doga" ]
+
+
+// object declaretion
+let muobj ={
+    name: "dixit",
+    age: "19",
+
+}
+
+// function  in variable in store
+const myfunction =function() {
+    
+console.log("hello wold");
+
+}
+
+
+console.log(typeof heros) // object 
+console.log(typeof muobj)  // object 
+console.log(typeof myfunction)  //output : object function
+
+
+
+
+
+
+
+
+
