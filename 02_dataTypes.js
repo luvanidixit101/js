5b1b51320d6330b904a1fea52915e11f784d65f4
luvanidixit101 <luvanidixit101@gmail.com>
2025-10-23 19:01:13 +0530

    02 datatype

---
+++ 02_dataTypes.js
@@ -0,0 +1,33 @@
+"use strict";
+
+//alert(3+3) // we are using nodejs, not browser
+
+console.log(3 + 3) // code readability should not be high
+
+let name = "dixit"
+let age = 19
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+
+// bigint 
+
+// string -> " "
+// boolean = true/false
+// null => standalone value 
+
+// undefined -> 
+//symbol-> unique
+
+//object 
+console.log(typeof undefined);
+console.log(typeof null);
+
+
+
+
+
+
+
+
