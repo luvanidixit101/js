dd9a7c095fb08b7faa10e5a7eebbe65f592d081c
luvanidixit101 <luvanidixit101@gmail.com>
2025-10-23 19:23:53 +0530

    03 datatype convergen

---
+++ 03_conversionOperation.js
@@ -0,0 +1,36 @@
+let score = "33@"
+let score1 = null
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+console.log(score1);
+
+// "33" => 33
+// "33abc" -> NaN
+// true -> 1 or false -> 0
+
+let isLoggedIn = 0
+let isLoggedIn1 = "dixit"
+
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+console.log(isLoggedIn1)
+
+
+//string
+someNumber = 33
+
+
+
+let stringNumber =String(someNumber)
+
+
+console.log(typeof someNumber)
+console.log(typeof stringNumber)
+console.log(stringNumber)
